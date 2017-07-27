2c07115193ee0e527d8c3a920db42b4495b62224
Kyrylo Yefimenko <kiriloman1234@hotmail.com>
2017-07-27 10:08:36 +0100

    hooks refactory.

--- gmail/cucumber/features/support/env.rb
+++ gmail/cucumber/features/support/env.rb
@@ -7,5 +7,5 @@ require 'selenium-webdriver'
 require 'site_prism'
 
 Capybara.register_driver :selenium do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :firefox)
+  Capybara::Selenium::Driver.new(app, :browser => :firefox, :marionette => true)
 end
--- gmail/cucumber/features/support/hooks.rb
+++ gmail/cucumber/features/support/hooks.rb
@@ -1,7 +1,11 @@
 Before do
-  Capybara.page.driver.browser.manage.window.maximize
   Capybara.configure do |config|
     Capybara.default_driver = :selenium
   end
+  Capybara.javascript_driver = :selenium
   Capybara.default_max_wait_time = 20
+end
+
+Before do
+  Capybara.page.driver.browser.manage.window.maximize
 end
