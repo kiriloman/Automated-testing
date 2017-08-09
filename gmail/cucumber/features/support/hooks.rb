007138b1d7f83e9474f512dfacf3e68e08576ab0
Kyrylo Yefimenko <kiriloman1234@hotmail.com>
2017-08-09 14:48:27 +0100

    upd

--- gmail/cucumber/features/support/hooks.rb
+++ gmail/cucumber/features/support/hooks.rb
@@ -3,11 +3,10 @@ require_relative 'helper.rb'
 World(Helper)
 
 Before do
-=begin
   Capybara.configure do |config|
     config.default_driver = :selenium
   end
-=end
+  Capybara.current_driver = :selenium
   Capybara.javascript_driver = :selenium
   Capybara.default_max_wait_time = 20
 end
